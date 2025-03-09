73c83dae284563cf00421d12aa6dff1bc55f0fe6
AbiXnash <abixnash@gmail.com>
2025-03-09 12:14:17 +0530

    Updated Footer

--- src/parts/Footer.js
+++ src/parts/Footer.js
@@ -5,51 +5,59 @@
 import React from "react";
 
 import BrandIcon from "./BrandIcon";
-import Button from "../elements/Button";
+// import Button from "../elements/Button";
 
 export default function Footer() {
   return (
-    <div className="bg-gray-50 border-t border-gray-200 pb-6">
-      <div className="container flex-col mx-auto ">
-        <div className="flex flex-col sm:flex-row mt-8 justify-center">
-          <div className="w-1/3 flex-col ml-16 mr-8">
+    <div className="bg-gray-50 border-t border-gray-200 py-8">
+      <div className="container mx-auto px-6">
+        <div className="flex flex-col sm:flex-row justify-between items-start gap-10">
+          {/* Column 1: Brand Info */}
+          <div className="sm:w-1/3 w-full">
             <BrandIcon />
-            <p className="w-full text-lg text-gray-400 font-light">
-              Growing Your Business <br />
-              Is Our Calling
+            <p className="text-lg text-gray-500 font-light mt-2">
+              Growing Your Business <br /> Is Our Calling
             </p>
           </div>
-          <div className="w-1/3 mt-0 ml-16 mr-0 sm:ml-0 sm:mr-5">
-            <h1 className="text-lg text-theme-blue pt-4 pb-2">Contact</h1>
-            <p className="text-lg text-gray-400 font-light">Thenmozhi </p>
-            <p className="text-lg text-gray-400 font-light">+91 8667608025</p>
-            <p className="text-lg text-gray-400 font-light">
-              Tamilnadu, India - 612001
+
+          {/* Column 2: Contact Info */}
+          <div className="sm:w-1/3 w-full">
+            <h1 className="text-lg text-theme-blue font-semibold">Contact</h1>
+            <p className="text-lg text-gray-500 font-light mt-1">Thenmozhi</p>
+            <p className="text-lg text-gray-500 font-light">+91 8667608025</p>
+            <p className="text-lg text-gray-500 font-light">
+              Tamil Nadu, India - 612001
             </p>
           </div>
-          <div className="w-1/3 ml-16 sm:ml-0 mt-0">
-            <h1 className="text-lg text-theme-blue pt-4 pb-2">Social</h1>
-            <Button
-              href="https://www.instagram.com/the._.nightingale"
-              type="link"
-              target="_blank"
-              className="flex text-lg text-gray-400 font-light hover:underline"
-              isExternal
-            >
-              Instagram
-            </Button>
-            <Button
-              href="https://www.linkedin.com/in/thenmozhi-t/"
-              type="link"
-              target="_blank"
-              className="flex text-lg text-gray-400 font-light hover:underline"
-              isExternal
-            >
-              LinkedIn
-            </Button>
+
+          {/* Column 3: Social Links */}
+          <div className="sm:w-1/3 w-full">
+            <h1 className="text-lg text-theme-blue font-semibold">Social</h1>
+            <div className="flex flex-col gap-2 mt-1">
+              <a
+                href="https://www.instagram.com/the._.nightingale"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-lg font-light hover:underline bg-clip-text text-transparent 
+                       bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"
+              >
+                Instagram
+              </a>
+              <a
+                href="https://www.linkedin.com/in/thenmozhi-t/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-lg font-light hover:underline bg-clip-text text-transparent 
+                       bg-gradient-to-r from-blue-500 to-blue-700"
+              >
+                LinkedIn
+              </a>
+            </div>
           </div>
         </div>
-        <div className="flex-col text-center mt-7">
+
+        {/* Footer */}
+        <div className="text-center mt-8">
           <p className="text-lg text-gray-400 font-light">
             Copyright 2025 - All rights reserved - The Globe
           </p>
